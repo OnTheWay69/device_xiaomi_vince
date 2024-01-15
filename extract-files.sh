b800ec3973c045f272ebf23296b2fd116648d1f2
Dutaiclik <nuraly042000@gmail.com>
2024-01-15 18:13:54 +0000

    vince: Switch to patchelf v0.17.2 for patching our blobs
    Signed-off-by: Dutaiclik <nuraly042000@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -9,15 +9,15 @@
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
-            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            "${PATCHELF_0_17_2}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so)
-            "${PATCHELF_0_8}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            "${PATCHELF_0_17_2}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
 	    ;;
     esac
 # For all ELF files
     if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
-        "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+        "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
     fi
 }
 
