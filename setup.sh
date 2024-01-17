bee382e7721b53985b1da9c5f9224402346cf21b
OnThe Way69 <gamecok515@gmail.com>
2024-01-17 20:07:19 +0000

    tes: setup
    Signed-off-by: OnThe Way69 <gamecok515@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/OnTheWay69/vendor_xiaomi_vince -b matrix vendor/xiaomi/vince
+git clone https://github.com/OnTheWay69/device_xiaomi_msm8953-common -b matrix device/xiaomi/msm8953-common
+git clone https://github.com/OnTheWay69/vendor_xiaomi_msm8953-common -b vendor/xiaomi/msm8953-common
+git clone https://github.com/Dutaiclik/kernel_xiaomi_vince -b clo kernel/xiaomi/msm8953
+git clone --depth=1 https://gitlab.com/cherishos-tiramisu/vince-clang-17.git prebuilts/clang/host/linux-x86/clang-aosp
